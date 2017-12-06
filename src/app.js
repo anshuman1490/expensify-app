3e31424370c6d3e3fac0ef89b558728315708571
anshuman1490 <anshuman.s1490@gmail.com>
2017-12-06 22:39:53 +0530

    add login/logout

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './router/AppRouter';
+import AppRouter, { history } from './router/AppRouter';
 import { Provider } from 'react-redux';
 
 
@@ -9,11 +9,10 @@ import './style/style.scss';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter, sortByAmount } from './actions/filters';
-import './firebase/firebase.js';
+import { login, logout } from './actions/auth';
+import { firebase } from './firebase/firebase.js';
 
 const store = configureStore();
-
 
 const Jsx = (
 	<Provider store={store}>
@@ -21,8 +20,28 @@ const Jsx = (
 	</Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+	if (!hasRendered) {
+		ReactDOM.render(Jsx, document.getElementById('app'));
+		hasRendered = true;
+	}
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-	ReactDOM.render(Jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		store.dispatch(login(user.uid));
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp();
+			if (history.location.pathname === '/') {
+				history.push('/dashboard');
+			}
+		});
+	} else {
+		store.dispatch(logout());
+		history.push('/');
+		renderApp();
+	}
 });
